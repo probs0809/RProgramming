9eb28985bd4143edf2f8e0adce923cfab5d01361
Prabodh Mayekar <prabodhmayekar@gmail.com>
2019-11-11 11:56:54 +0530

    initial commit

---
+++ Sql.R
@@ -0,0 +1,5 @@
+library(sqldf);
+sqldf("SELECT * FROM BOD WHERE Time = 7");
+sqldf("SELECT * FROM mtcars WHERE hp = 93");
+
+
---
+++ fubunnacci.R
@@ -0,0 +1,9 @@
+Fibonacci <- function(n) {
+    x <- c(0,1)
+    while (length(x) < n) {
+        position <- length(x)
+        new <- x[position] + x[position-1]
+        x <- c(x,new)
+    }
+    return(x)
+}
